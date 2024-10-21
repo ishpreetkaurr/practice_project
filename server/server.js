8e54be7326552d1d449592185b33f73cba5cb400
ishpreetkaurr <ishpreetarora01@gmail.com>
2024-10-21 11:50:50 +0530

    framework configuration

---
+++ server/server.js
@@ -0,0 +1,26 @@
+//FRAMEWORK CONFIGURATION:
+
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
+
+connectDb();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+//routes below
+app.get('/',(req,res)=>{
+    res.send("working");
+});
+
+//Error handling middleware
+app.use(errorHandler());
+
+//APP CONFIG START
+app.listen(port, () => {
+console.log(`Server running on port http://localhost:${port}`);
+});
