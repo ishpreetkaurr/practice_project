aadd9b3d1eee5b1434d72d29eb9b74420ffd7be1
ishpreetkaurr <ishpreetarora01@gmail.com>
2024-10-23 11:35:41 +0530

    Handlebars(day3)

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ dotenv.config();
 
 connectDb();
 const app = express();
+app.set('view engine', 'hbs');
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -19,6 +20,20 @@ app.use(cors());
 //routes below
 app.get('/',(req,res)=>{
     res.send("working");
+});
+
+app.get('/home',(req,res)=>{
+    //let user = User.findOne({id:})
+    res.render("home",{
+        username:"Carlos",
+        posts:"Spain, Italy"
+    })
+});
+
+app.get('/allUsers',(req,res)=>{
+    const data = [{name: "Charles", team: "Ferrari"},
+    {name:"Oscar", team:"McLaren"}];
+    res.render("home",{data});
 });
 
 //Error handling middleware
